cfc876581d66f639705c9f239b6ff151ccb05319
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-20 14:11:59 +0100

    Create Post component for Feed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,11 +1,17 @@
 import './feed.css'
 import Share from '../share/Share'
+import Post from '../post/Post'
 
 const Feed = () => {
   return (
     <div className='feed'>
       <div className="feed-wrapper">
         <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   )
