6efe4be17f73297efd163fa831c1628ea09dd05e
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-20 14:08:13 +0100

    Create Share component for Feed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,8 +1,13 @@
 import './feed.css'
+import Share from '../share/Share'
 
 const Feed = () => {
   return (
-    <div className='feed'>Feed</div>
+    <div className='feed'>
+      <div className="feed-wrapper">
+        <Share />
+      </div>
+    </div>
   )
 }
 
