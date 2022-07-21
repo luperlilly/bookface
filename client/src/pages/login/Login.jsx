9900f11f6e3ea0830113e6ebaedebc97ee93289d
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-21 11:19:37 +0100

    Handle login/register form data entry

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react'
 import './login.css'
 
 const Login = () => {
+  const [data, setData] = useState({ email: "", password: "" })
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+
   return (
     <div className='login'>
       <div className="login-wrapper">
@@ -12,8 +19,8 @@ const Login = () => {
         </div>
         <div className="login-right">
           <div className="login-box">
-            <input type="email" placeholder="Email" className="login-input" />
-            <input type="password" placeholder="Password" className="login-input" />
+            <input type="email" placeholder="Email" className="login-input" name='email' onChange={handleChange} />
+            <input type="password" placeholder="Password" className="login-input" name='password' onChange={handleChange} />
             <button className="login-button">Log in</button>
             <span className="login-forgot">Forgot password?</span>
             <button className="login-register-button">Create a new account</button>
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react'
 import './register.css'
 
 const Register = () => {
+  const [data, setData] = useState({ username: "", email: "", password: "", confirmpassword: "" })
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+
   return (
     <div className='login'>
       <div className="login-wrapper">
@@ -12,10 +19,10 @@ const Register = () => {
         </div>
         <div className="login-right">
           <div className="login-box">
-            <input placeholder="Username" className="login-input" />
-            <input type="email" placeholder="Email" className="login-input" />
-            <input type="password" placeholder="Password" className="login-input" />
-            <input type="password" placeholder="Confirm password" className="login-input" />
+            <input placeholder="Username" className="login-input" name='username' onChange={handleChange} />
+            <input type="email" placeholder="Email" className="login-input" name='email' onChange={handleChange} />
+            <input type="password" placeholder="Password" className="login-input" name='password' onChange={handleChange} />
+            <input type="password" placeholder="Confirm password" className="login-input" name='confirmpassword' onChange={handleChange} />
             <button className="login-button">Sign up</button>
             <button className="login-register-button">Already have an account?</button>
           </div>
