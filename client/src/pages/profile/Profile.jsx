571f3af1e94511abd144f954f7e74aea22d4143e
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-22 13:11:29 +0100

    User data displays on Profile

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -2,9 +2,11 @@ import Feed from '../../components/feed/Feed'
 import Leftbar from '../../components/leftbar/Leftbar'
 import Rightbar from '../../components/rightbar/Rightbar'
 import Topbar from '../../components/topbar/Topbar'
+import { useSelector } from 'react-redux'
 import './profile.css'
 
 const Profile = () => {
+  const user  = useSelector((state) => state.authReducer.authData)
   const PF = process.env.REACT_APP_PUBLIC_FOLDER
 
   return (
@@ -15,12 +17,12 @@ const Profile = () => {
         <div className="profile-right">
           <div className="profile-right-top">
             <div className="profile-cover">
-              <img className='profile-cover-image' src={`${PF}default-cover.jpeg`} alt="" />
-              <img className='profile-user-image' src={`${PF}default-profile.png`} alt="" />
+              <img className='profile-cover-image' src={user.coverPicture ? PF + user.coverPicture : PF + 'default-cover.jpeg'} alt="" />
+              <img className='profile-user-image' src={user.profilePicture ? PF + user.profilePicture : PF + 'default-profile.png'} alt="" />
             </div>
             <div className="profile-info">
-              <h4 className='profile-info-name'>Bobson Dugnutt</h4>
-              <span className='profile-info-description'>This be Bobson</span>
+              <h4 className='profile-info-name'>{user.username}</h4>
+              <span className='profile-info-description'>{user.description}</span>
             </div>
           </div>
           <div className="profile-right-bottom">
