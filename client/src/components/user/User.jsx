c36bf78ae8127751a3415c760f393dbc9065e2c5
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-23 08:00:57 +0100

    People you may know on Leftbar

---
+++ client/src/components/user/User.jsx
@@ -0,0 +1,17 @@
+import './user.css'
+import { Link } from 'react-router-dom'
+
+const User = ({ person }) => {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER
+
+  return (
+    <Link to={`../profile/${person._id}`} style={{ textDecoration: "none", color: "inherit" }}>
+      <div className='user'>
+        <img className='user-image' src={person.profilePicture ? PF + person.profilePicture : PF + 'default-profile.png'} alt="" />
+        <span className="user-name">{person.username}</span>
+      </div>
+    </Link>
+  )
+}
+
+export default User
