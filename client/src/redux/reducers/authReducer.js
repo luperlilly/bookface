5cd74648f7490ef4e4bdb2ac63c5654c0fd63955
Alex Mendes <alexanderhmendes@gmail.com>
2022-07-23 15:59:47 +0100

    fix: get following/unfollowing working

--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -1,5 +1,5 @@
 const authReducer = (
-  state = { authData: null, loading: false, error: false, updateLoading: false }, 
+  state = { authData: null, loading: false, error: false, updateLoading: false },
   action) => {
   switch (action.type) {
     case "AUTH_START":
@@ -11,15 +11,15 @@ const authReducer = (
       return { ...state, loading: false, error: true }
     case "UPDATING_START":
       return { ...state, updateLoading: true, error: false }
-    case "UPDATING_SUCCESS":        
+    case "UPDATING_SUCCESS":
       localStorage.setItem("profile", JSON.stringify({...action?.data}))
       return { ...state, authData: action.data, updateLoading: false, error: false}
     case "UPDATING_FAIL":
       return { ...state, updateLoading: true, error: true}
     case "FOLLOW_USER":
-      return { ...state, authData: {...state.authData, user: {...state.authData.user, following: [...state.authData.user.following, action.data]} }}
+      return { ...state, authData: {...state.authData, following: [...state.authData.following, action.data]} }
     case "UNFOLLOW_USER":
-      return { ...state, authData: {...state.authData, user: {...state.authData.user, following: [...state.authData.user.following?.filter((personId) => personId !== action.data)] } }}
+      return { ...state, authData: { ...state.authData, following: [...state.authData.following?.filter((personId) => personId !== action.data)] } }
     case "LOG_OUT":
       localStorage.clear()
       return { ...state, authData: null, loading: false, error: false }
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js'
 import { createError } from "../error.js"
 import bcrypt from 'bcrypt'
+import { getCurrentUser } from '../auth.js'
 
 export const updateUser = async (req, res, next) => {
   if (req.body._id === req.params.id || req.body.isAdmin) {
@@ -16,7 +17,7 @@ export const updateUser = async (req, res, next) => {
 
     try {
       const updatedUser = await User.findByIdAndUpdate(
-        req.params.id, 
+        req.params.id,
         {
           $set: req.body
         },
@@ -42,7 +43,7 @@ export const deleteUser = async (req, res, next) => {
   } else {
     return next(createError(403, 'You can only delete your own account'))
   }
-} 
+}
 
 export const getUser = async (req, res, next) => {
   try {
@@ -55,44 +56,64 @@ export const getUser = async (req, res, next) => {
 }
 
 export const followUser = async (req, res, next) => {
-  if (req.body.userId !== req.params.id) {
-    try {
-      const user = await User.findById(req.params.id)
-      const currentUser = await User.findById(req.body.userId)
-      if (!user.followers.includes(req.body.userId)) {
-        await user.updateOne({ $push: {followers: req.body.userId }})
-        await currentUser.updateOne({ $push: {following: req.params.id }})
-        res.status(200).json("User followed!")
-      } else {
-        return next(createError(403, 'You already follow this user'))
-      }
-    } catch (error) {
-      next(error)
-    }
-  } else {
+  const currentUser = await getCurrentUser(req)
+  const userToFollow = await User.findById(req.params.id);
+
+  if (!userToFollow) {
+    return next(createError(400, 'No user could be found with this ID'))
+  }
+
+  const currentUserId = currentUser._id.toString();
+  const userToFollowId = userToFollow._id.toString();
+
+  if (currentUserId === userToFollowId) {
     return next(createError(403, 'You cannot follow yourself'))
   }
-} 
+
+  if (userToFollow.followers.includes(currentUserId)) {
+    await currentUser.updateOne({ $push: { following: userToFollowId }})
+
+    return next(createError(400, 'You already follow this user'))
+  }
+
+  try {
+    await userToFollow.updateOne({ $push: { followers: currentUserId }})
+    await currentUser.updateOne({ $push: { following: userToFollowId }})
+    res.status(200).json("User followed!")
+  } catch (error) {
+    next(error)
+  }
+}
 
 export const unfollowUser = async (req, res, next) => {
-  if (req.body.userId !== req.params.id) {
-    try {
-      const user = await User.findById(req.params.id)
-      const currentUser = await User.findById(req.body.userId)
-      if (user.followers.includes(req.body.userId)) {
-        await user.updateOne({ $pull: {followers: req.body.userId }})
-        await currentUser.updateOne({ $pull: {following: req.params.id }})
-        res.status(200).json("User unfollowed")
-      } else {
-        return next(createError(403, 'You do not follow this user'))
-      }
-    } catch (error) {
-      next(error)
-    }
-  } else {
+  const currentUser = await getCurrentUser(req)
+  const userToUnfollow = await User.findById(req.params.id);
+
+  if (!userToUnfollow) {
+    return next(createError(400, 'No user could be found with this ID'))
+  }
+
+  const currentUserId = currentUser._id.toString();
+  const userToUnfollowId = userToUnfollow._id.toString();
+
+  if (currentUserId === userToUnfollowId) {
     return next(createError(403, 'You cannot unfollow yourself'))
   }
-} 
+
+  if (!userToUnfollow.followers.includes(currentUserId)) {
+    await currentUser.updateOne({ $pull: { following: userToUnfollowId }})
+
+    return next(createError(400, 'You do not follow this user'))
+  }
+
+  try {
+    await userToUnfollow.updateOne({ $pull: { followers: currentUserId }})
+    await currentUser.updateOne({ $pull: { following: userToUnfollowId }})
+    res.status(200).json("User unfollowed!")
+  } catch (error) {
+    next(error)
+  }
+}
 
 export const getAllUsers = async (req, res, next) => {
   try {
