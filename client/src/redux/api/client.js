24d8f872c7bb3048af3e8bd284a2fa877abd6640
Alex Mendes <alexanderhmendes@gmail.com>
2022-07-23 12:58:52 +0100

    docs: add comment about auth header

--- client/src/redux/api/client.js
+++ client/src/redux/api/client.js
@@ -6,6 +6,9 @@ const API = axios.create({
   withCredentials: true,
 })
 
+// This isn't actually being used as the server is just looking for the access_token
+// cookie directly. Another approach would indeed be to submit an Authorization
+// header. Probably just choose one and get rid of everything about the other!
 API.interceptors.request.use((req) => {
   const accessToken = cookie.get('access_token')
 
